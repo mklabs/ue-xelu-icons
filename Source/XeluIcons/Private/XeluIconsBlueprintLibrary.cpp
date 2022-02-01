f0e7bc4cfbd7e117555ff1058e4a460ceeb3c2cc
Mickael Daniel <daniel.mickael@gmail.com>
2022-02-01 17:20:23 +0100

    feat: Add `GetSoftIconsForActionMapping()` and `GetSoftIconsForActionMappingWithModifiers()`
    As well as singular versions returning first matched key for given Input Action name.

--- Source/XeluIcons/Private/XeluIconsBlueprintLibrary.cpp
+++ Source/XeluIcons/Private/XeluIconsBlueprintLibrary.cpp
@@ -7,6 +7,7 @@
 #include "XeluIconsDeveloperSettings.h"
 #include "XeluIconsInputMetadata.h"
 #include "XeluIconsLog.h"
+#include "GameFramework/InputSettings.h"
 
 const UXeluIconsDeveloperSettings* UXeluIconsBlueprintLibrary::GetPluginSettings()
 {
@@ -24,6 +25,39 @@ const UDataTable* UXeluIconsBlueprintLibrary::GetIconsDatatable()
 	}
 
 	return DataTable.Get();
+}
+
+UTexture2D* UXeluIconsBlueprintLibrary::GetIconTextureForKey(const FKey Key, const EXeluIconsIconType IconPreference)
+{
+	const TSoftObjectPtr<UTexture2D> Icon = GetSoftIconTextureForKey(Key, IconPreference);
+	return Icon.IsValid() ? Icon.Get() : nullptr;
+}
+
+TSoftObjectPtr<UTexture2D> UXeluIconsBlueprintLibrary::GetSoftIconTextureForKey(const FKey Key, EXeluIconsIconType IconPreference, const bool bShouldLoadSynchronous)
+{
+	const UDataTable* IconsDatatable = GetIconsDatatable();
+
+	// Check if passed in key is a keyboard / mouse and force icon preference to keyboard if it is
+	const bool bIsGamepadKey = Key.IsGamepadKey();
+	if (bIsGamepadKey && IconPreference == EXeluIconsIconType::Keyboard)
+	{
+		// Fallback to Xbox one icon type when wrong preference given for gamepad key
+		IconPreference = EXeluIconsIconType::XboxOne;
+	}
+	else if (!bIsGamepadKey)
+	{
+		// Force Keyboard icon type for any key non gamepad
+		IconPreference = EXeluIconsIconType::Keyboard;
+	}
+
+	const TSoftObjectPtr<UTexture2D> Texture2D = GetIconTextureInternal(Key, IconsDatatable, IconPreference, bShouldLoadSynchronous);
+	if (Texture2D.IsNull())
+	{
+		XI_LOG(Error, TEXT("GetSoftIconTextureForKey: Texture2D soft pointer is null and can never point to a live UObject"))
+		return nullptr;
+	}
+
+	return Texture2D;
 }
 
 UTexture2D* UXeluIconsBlueprintLibrary::GetIconTextureForInputAction(UInputAction* InputAction, UInputMappingContext* MappingContext, const EXeluIconsIconType IconPreference)
@@ -89,37 +123,180 @@ TSoftObjectPtr<UTexture2D> UXeluIconsBlueprintLibrary::GetSoftIconTextureForInpu
 	return MatchingIcon;
 }
 
-UTexture2D* UXeluIconsBlueprintLibrary::GetIconTextureForKey(const FKey Key, const EXeluIconsIconType IconPreference)
+TSoftObjectPtr<UTexture2D> UXeluIconsBlueprintLibrary::GetSoftIconForActionMapping(const FName InputName, const EXeluIconsIconType IconPreference, const bool bShouldLoadSynchronous)
 {
-	const TSoftObjectPtr<UTexture2D> Icon = GetSoftIconTextureForKey(Key, IconPreference);
-	return Icon.IsValid() ? Icon.Get() : nullptr;
+	TSoftObjectPtr<UTexture2D> MatchingIcon = nullptr;
+
+	const UInputSettings* InputSettings = UInputSettings::GetInputSettings();
+
+	// First, get all action names and check for validity of passed in input name
+	TArray<FName> ActionNames;
+	InputSettings->GetActionNames(ActionNames);
+
+	if (!ActionNames.Contains(InputName))
+	{
+		XI_LOG(Warning, TEXT("GetSoftIconForActionMapping: Input with name '%s' couldn't be found in Input Settings."))
+		return MatchingIcon;
+	}
+
+	TArray<TSoftObjectPtr<UTexture2D>> Icons = GetSoftIconsForActionMapping(InputName, IconPreference, bShouldLoadSynchronous);
+
+	// Only return the first icon found here, if any matching icons
+	if (Icons.IsValidIndex(0))
+	{
+		MatchingIcon = Icons[0];
+	}
+
+	return MatchingIcon;
 }
 
-TSoftObjectPtr<UTexture2D> UXeluIconsBlueprintLibrary::GetSoftIconTextureForKey(const FKey Key, EXeluIconsIconType IconPreference, const bool bShouldLoadSynchronous)
+TArray<TSoftObjectPtr<UTexture2D>> UXeluIconsBlueprintLibrary::GetSoftIconsForActionMapping(FName InputName, EXeluIconsIconType IconPreference, const bool bShouldLoadSynchronous)
 {
+	TArray<TSoftObjectPtr<UTexture2D>> MatchingIcons;
+
 	const UDataTable* IconsDatatable = GetIconsDatatable();
+	const UInputSettings* InputSettings = UInputSettings::GetInputSettings();
 
-	// Check if passed in key is a keyboard / mouse and force icon preference to keyboard if it is
-	const bool bIsGamepadKey = Key.IsGamepadKey();
-	if (bIsGamepadKey && IconPreference == EXeluIconsIconType::Keyboard)
+	// First, get all action names and check for validity of passed in input name
+	TArray<FName> ActionNames;
+	InputSettings->GetActionNames(ActionNames);
+
+	if (!ActionNames.Contains(InputName))
 	{
-		// Fallback to Xbox one icon type when wrong preference given for gamepad key
-		IconPreference = EXeluIconsIconType::XboxOne;
-	}
-	else if (!bIsGamepadKey)
-	{
-		// Force Keyboard icon type for any key non gamepad
-		IconPreference = EXeluIconsIconType::Keyboard;
+		XI_LOG(Warning, TEXT("GetSoftIconsForActionMapping: Input with name '%s' couldn't be found in Input Settings."))
+		return MatchingIcons;
 	}
 
-	const TSoftObjectPtr<UTexture2D> Texture2D = GetIconTextureInternal(Key, IconsDatatable, IconPreference, bShouldLoadSynchronous);
-	if (Texture2D.IsNull())
+	const TArray<FInputActionKeyMapping> ActionMappings = InputSettings->GetActionMappings();
+
+	// Find all Action Mappings defining the provided input name
+	TArray<FInputActionKeyMapping> FoundActionMappings = ActionMappings.FilterByPredicate([InputName, IconPreference](const FInputActionKeyMapping& Mapping)
 	{
-		XI_LOG(Error, TEXT("GetSoftIconTextureForKey: Texture2D soft pointer is null and can never point to a live UObject"))
-		return nullptr;
+		const bool bIsGamepadKey = Mapping.Key.IsGamepadKey();
+		const bool bIsValidKey = IconPreference == EXeluIconsIconType::Keyboard && !bIsGamepadKey || IconPreference != EXeluIconsIconType::Keyboard && bIsGamepadKey;
+
+		if (!bIsValidKey)
+		{
+			return false;
+		}
+
+		return Mapping.ActionName == InputName;
+	});
+
+	// We have matching actions, find texture for the keys and return that
+	if (FoundActionMappings.Num() > 0)
+	{
+		for (const FInputActionKeyMapping ActionMapping : FoundActionMappings)
+		{
+			TSoftObjectPtr<UTexture2D> Icon = GetIconTextureInternal(ActionMapping.Key, IconsDatatable, IconPreference, bShouldLoadSynchronous);
+			if (!Icon.IsNull())
+			{
+				MatchingIcons.AddUnique(Icon);
+			}
+		}
 	}
 
-	return Texture2D;
+	return MatchingIcons;
+}
+
+FXeluIconsActionMappingIcons UXeluIconsBlueprintLibrary::GetSoftIconForActionMappingWithModifiers(const FName InputName, const EXeluIconsIconType IconPreference, const bool bShouldLoadSynchronous)
+{
+	FXeluIconsActionMappingIcons MatchingMappingIcons = FXeluIconsActionMappingIcons();
+
+	const UInputSettings* InputSettings = UInputSettings::GetInputSettings();
+
+	// First, get all action names and check for validity of passed in input name
+	TArray<FName> ActionNames;
+	InputSettings->GetActionNames(ActionNames);
+
+	if (!ActionNames.Contains(InputName))
+	{
+		XI_LOG(Warning, TEXT("GetSoftIconForActionMappingWithModifiers: Input with name '%s' couldn't be found in Input Settings."))
+		return FXeluIconsActionMappingIcons();
+	}
+
+	TArray<FXeluIconsActionMappingIcons> MappingIcons = GetSoftIconsForActionMappingWithModifiers(InputName, IconPreference, bShouldLoadSynchronous);
+
+	// Only return the first icon found here, if any matching icons
+	if (MappingIcons.IsValidIndex(0))
+	{
+		MatchingMappingIcons = MappingIcons[0];
+	}
+
+	return MatchingMappingIcons;
+}
+
+TArray<FXeluIconsActionMappingIcons> UXeluIconsBlueprintLibrary::GetSoftIconsForActionMappingWithModifiers(FName InputName, EXeluIconsIconType IconPreference, bool bShouldLoadSynchronous)
+{
+	TArray<FXeluIconsActionMappingIcons> ActionMappingIcons;
+
+	const UDataTable* IconsDatatable = GetIconsDatatable();
+	const UInputSettings* InputSettings = UInputSettings::GetInputSettings();
+
+	// First, get all action names and check for validity of passed in input name
+	TArray<FName> ActionNames;
+	InputSettings->GetActionNames(ActionNames);
+
+	if (!ActionNames.Contains(InputName))
+	{
+		XI_LOG(Warning, TEXT("GetSoftIconsForActionMappingWithModifiers: Input with name '%s' couldn't be found in Input Settings."))
+		return ActionMappingIcons;
+	}
+
+	const TArray<FInputActionKeyMapping> ActionMappings = InputSettings->GetActionMappings();
+
+	// Find all Action Mappings defining the provided input name
+	TArray<FInputActionKeyMapping> FoundActionMappings = ActionMappings.FilterByPredicate([InputName, IconPreference](const FInputActionKeyMapping& Mapping)
+	{
+		const bool bIsGamepadKey = Mapping.Key.IsGamepadKey();
+		const bool bIsValidKey = IconPreference == EXeluIconsIconType::Keyboard && !bIsGamepadKey || IconPreference != EXeluIconsIconType::Keyboard && bIsGamepadKey;
+
+		if (!bIsValidKey)
+		{
+			return false;
+		}
+
+		return Mapping.ActionName == InputName;
+	});
+
+	// We have matching actions, find texture for the keys and return that
+	if (FoundActionMappings.Num() > 0)
+	{
+		for (const FInputActionKeyMapping ActionMapping : FoundActionMappings)
+		{
+			FXeluIconsActionMappingIcons MappingIcon;
+			MappingIcon.Key = ActionMapping.Key;
+			MappingIcon.bAlt = ActionMapping.bAlt;
+			MappingIcon.bCmd = ActionMapping.bCmd;
+			MappingIcon.bCtrl = ActionMapping.bCtrl;
+			MappingIcon.bShift = ActionMapping.bShift;
+
+			MappingIcon.KeyIcon = GetIconTextureInternal(ActionMapping.Key, IconsDatatable, IconPreference, bShouldLoadSynchronous);
+			if (MappingIcon.bAlt)
+			{
+				MappingIcon.AltIcon = GetIconTextureInternal(EKeys::LeftAlt, IconsDatatable, IconPreference, bShouldLoadSynchronous);
+			}
+
+			if (MappingIcon.bCmd)
+			{
+				MappingIcon.CmdIcon = GetIconTextureInternal(EKeys::LeftCommand, IconsDatatable, IconPreference, bShouldLoadSynchronous);
+			}
+
+			if (MappingIcon.bCtrl)
+			{
+				MappingIcon.CtrlIcon = GetIconTextureInternal(EKeys::LeftControl, IconsDatatable, IconPreference, bShouldLoadSynchronous);
+			}
+
+			if (MappingIcon.bShift)
+			{
+				MappingIcon.ShiftIcon = GetIconTextureInternal(EKeys::LeftShift, IconsDatatable, IconPreference, bShouldLoadSynchronous);
+			}
+
+			ActionMappingIcons.Add(MappingIcon);
+		}
+	}
+
+	return ActionMappingIcons;
 }
 
 TSoftObjectPtr<UTexture2D> UXeluIconsBlueprintLibrary::GetIconTextureInternal(const FKey Key, const UDataTable* IconsDatatable, const EXeluIconsIconType IconPreference, const bool bShouldLoadSynchronous)
--- Source/XeluIcons/Public/XeluIconsBlueprintLibrary.h
+++ Source/XeluIcons/Public/XeluIconsBlueprintLibrary.h
@@ -12,6 +12,54 @@ class UInputAction;
 class UInputMappingContext;
 class UXeluIconsDeveloperSettings;
 
+USTRUCT(BlueprintType)
+struct FXeluIconsActionMappingIcons
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(BlueprintReadOnly, Category = "Xelu Icons")
+	FKey Key;
+
+	/** Set to matching icon for the key */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Xelu Icons")
+	TSoftObjectPtr<UTexture2D> KeyIcon = nullptr;
+
+	/** true if one of the Shift keys must be down when the KeyEvent is received to be acknowledged */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Xelu Icons")
+	bool bShift = false;
+
+	/** Set if one of the Shift keys must be down when the KeyEvent is received to be acknowledged */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Xelu Icons")
+	TSoftObjectPtr<UTexture2D> ShiftIcon = nullptr;
+
+	/** true if one of the Ctrl keys must be down when the KeyEvent is received to be acknowledged */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Xelu Icons")
+	bool bCtrl = false;
+
+	/** Set if one of the Ctrl keys must be down when the KeyEvent is received to be acknowledged */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Xelu Icons")
+	TSoftObjectPtr<UTexture2D> CtrlIcon = nullptr;
+
+	/** true if one of the Alt keys must be down when the KeyEvent is received to be acknowledged */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Xelu Icons")
+	bool bAlt = false;
+
+	/** Set if one of the Alt keys must be down when the KeyEvent is received to be acknowledged */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Xelu Icons")
+	TSoftObjectPtr<UTexture2D> AltIcon = nullptr;
+
+	/** true if one of the Cmd keys must be down when the KeyEvent is received to be acknowledged */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Xelu Icons")
+	bool bCmd = false;
+
+	/** Set if one of the Cmd keys must be down when the KeyEvent is received to be acknowledged */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Xelu Icons")
+	TSoftObjectPtr<UTexture2D> CmdIcon = nullptr;
+};
+
+
 /**
  * Main library interface for Xelu Icons plugin
  */
@@ -27,21 +75,33 @@ public:
 	/** Returns Icons Datatable mapping from plugin settings */
 	static const UDataTable* GetIconsDatatable();
 
-	/** Returns matching icon texture given the passed in Input Action / Mapping Context, if there is a mapping defined for this configuration */
-	UFUNCTION(BlueprintPure, Category = "Xelu Icons")
-	static UTexture2D* GetIconTextureForInputAction(UInputAction* InputAction, UInputMappingContext* MappingContext, EXeluIconsIconType IconPreference = EXeluIconsIconType::Keyboard);
-
-	/** Returns matching icon texture as soft object pointer given the passed in Input Action / Mapping Context, if there is a mapping defined for this configuration */
-	UFUNCTION(BlueprintPure, Category = "Xelu Icons", Meta = (AdvancedDisplay = "bShouldLoadSynchronous"))
-	static TSoftObjectPtr<UTexture2D> GetSoftIconTextureForInputAction(UInputAction* InputAction, UInputMappingContext* MappingContext, EXeluIconsIconType IconPreference = EXeluIconsIconType::Keyboard, bool bShouldLoadSynchronous = true);
-
 	/** Returns matching icon texture for the given key. IconPreference is only relevant for gamepad keys */
-	UFUNCTION(BlueprintPure, Category = "Xelu Icons")
+	UFUNCTION(BlueprintPure, Category = "Xelu Icons | Key")
 	static UTexture2D* GetIconTextureForKey(FKey Key, EXeluIconsIconType IconPreference = EXeluIconsIconType::Keyboard);
 
 	/** Returns matching icon texture as soft object pointer for the given key. IconPreference is only relevant for gamepad keys */
-	UFUNCTION(BlueprintPure, Category = "Xelu Icons")
+	UFUNCTION(BlueprintPure, Category = "Xelu Icons | Key")
 	static TSoftObjectPtr<UTexture2D> GetSoftIconTextureForKey(FKey Key, EXeluIconsIconType IconPreference = EXeluIconsIconType::Keyboard, bool bShouldLoadSynchronous = true);
+
+	/** Returns matching icon texture given the passed in Input Action / Mapping Context, if there is a mapping defined for this configuration */
+	UFUNCTION(BlueprintPure, Category = "Xelu Icons | Enhanced Input")
+	static UTexture2D* GetIconTextureForInputAction(UInputAction* InputAction, UInputMappingContext* MappingContext, EXeluIconsIconType IconPreference = EXeluIconsIconType::Keyboard);
+
+	/** Returns matching icon texture as soft object pointer given the passed in Input Action / Mapping Context, if there is a mapping defined for this configuration */
+	UFUNCTION(BlueprintPure, Category = "Xelu Icons | Enhanced Input")
+	static TSoftObjectPtr<UTexture2D> GetSoftIconTextureForInputAction(UInputAction* InputAction, UInputMappingContext* MappingContext, EXeluIconsIconType IconPreference = EXeluIconsIconType::Keyboard, bool bShouldLoadSynchronous = true);
+
+	UFUNCTION(BlueprintPure, Category = "Xelu Icons | Input Settings")
+	static TSoftObjectPtr<UTexture2D> GetSoftIconForActionMapping(FName InputName, EXeluIconsIconType IconPreference = EXeluIconsIconType::Keyboard, bool bShouldLoadSynchronous = true);
+
+	UFUNCTION(BlueprintPure, Category = "Xelu Icons | Input Settings")
+	static TArray<TSoftObjectPtr<UTexture2D>> GetSoftIconsForActionMapping(FName InputName, EXeluIconsIconType IconPreference = EXeluIconsIconType::Keyboard, bool bShouldLoadSynchronous = true);
+
+	UFUNCTION(BlueprintPure, Category = "Xelu Icons | Input Settings")
+	static FXeluIconsActionMappingIcons GetSoftIconForActionMappingWithModifiers(FName InputName, EXeluIconsIconType IconPreference = EXeluIconsIconType::Keyboard, bool bShouldLoadSynchronous = true);
+
+	UFUNCTION(BlueprintPure, Category = "Xelu Icons | Input Settings")
+	static TArray<FXeluIconsActionMappingIcons> GetSoftIconsForActionMappingWithModifiers(FName InputName, EXeluIconsIconType IconPreference = EXeluIconsIconType::Keyboard, bool bShouldLoadSynchronous = true);
 
 	/** Main lookup helper to return the matching icon texture for the passed in key */
 	static TSoftObjectPtr<UTexture2D> GetIconTextureInternal(FKey Key, const UDataTable* IconsDatatable, EXeluIconsIconType IconPreference, bool bShouldLoadSynchronous = true);
